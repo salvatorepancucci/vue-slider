39bf92bd4f08692f2371cef691b817170501fb90
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-17 14:29:39 +0200

    Current slide index added on data

--- js/script.js
+++ js/script.js
@@ -11,7 +11,8 @@ createApp({
                 { img: 'img/04.webp', title: 'Stray', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' },
                 { img: 'img/05.webp', title: 'Avengers', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' }
             ],
-            
+            // Indice della slide corrente
+            currentSlide: 0
         };
     },
     methods: {
