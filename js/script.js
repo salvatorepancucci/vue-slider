12234e6fbb0cf0c2af327486d723a3aba4b7df86
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-17 14:33:39 +0200

    Bonus 2: autoplay added

--- js/script.js
+++ js/script.js
@@ -12,8 +12,14 @@ createApp({
                 { img: 'img/05.webp', title: 'Avengers', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' }
             ],
             // Indice della slide corrente
-            currentSlide: 0
+            currentSlide: 0,
+            // ID del timer di autoplay
+            autoplayTimer: null
         };
+    },
+    mounted() {
+        // Avvia l'autoplay quando il componente è montato
+        this.startAutoplay();
     },
     methods: {
         // Metodo per andare alla slide precedente
@@ -27,6 +33,10 @@ createApp({
         // Metodo per andare a una specifica slide
         goToSlide(index) {
             this.currentSlide = index;
-        }
+        },
+        // Metodo per avviare l'autoplay
+        startAutoplay() {
+            this.autoplayTimer = setInterval(this.nextSlide, 3000);
+        },
     }
 }).mount('#app');
