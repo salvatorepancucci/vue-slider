e1c92100fa4f9bf1e8ce60137bb4776183a3e561
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-17 14:29:19 +0200

    Objects array added to data

--- js/script.js
+++ js/script.js
@@ -3,10 +3,18 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            // Array di slide con immagini, titoli e descrizioni
+            slides: [
+                { img: 'img/01.webp', title: 'Spiderman', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' },
+                { img: 'img/02.webp', title: 'Ratchet & Clank', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' },
+                { img: 'img/03.webp', title: 'Fortnite', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' },
+                { img: 'img/04.webp', title: 'Stray', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' },
+                { img: 'img/05.webp', title: 'Avengers', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' }
+            ],
             
-        }
-    }
+        };
+    },
     methods: {
-
+        
     }
-}).mount('#app')
+}).mount('#app');
